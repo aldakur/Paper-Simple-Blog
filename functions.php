638af923063efd214c55317e98965572430fc7b6
aldakur <aldakur@gmail.com>
2017-05-31 14:59:56 +0200

    Chance to update theme from the GitHub repo

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ register_nav_menus( array(
 /* Theme setup */
 add_action( 'after_setup_theme', 'wpt_setup' );
     if ( ! function_exists( 'wpt_setup' ) ):
-        function wpt_setup() {  
+        function wpt_setup() {
             register_nav_menu( 'primary', __( 'Primary navigation', 'wptuts' ) );
         } endif;
 
@@ -29,7 +29,7 @@ register_sidebar(array (
         'class' => 'list-group'
      ));
 
-/* MY PLUGINS */
+// === MY PLUGINS === //
 
 // Bootstrap-widgets
 require_once('plugins/link-manager/link-manager.php');
@@ -44,6 +44,41 @@ require_once('plugins/bootstrap-widgets.php');
 
 // Register my plugins for archive widget
 require_once('widgets/paper-simple-blog-archive-widget.php');
+
+// === //
+
+
+
+/**
+  * Update theme from the GitHub
+  * https://linuxgnublog.org/es/como-actualizar-temas-custom-de-wordpress-desde-github/
+  */
+// set_site_transient('update_themes', null);
+function geko_check_update( $transient ) {
+  if ( empty( $transient->checked ) ) {
+    return $transient;
+  }
+  $theme_data = wp_get_theme(wp_get_theme()->template);
+  $theme_slug = $theme_data->get_template();
+  //Delete '-master' from the end of slug
+  $theme_uri_slug = preg_replace('/-master$/', '', $theme_slug);
+
+  $remote_version = '0.0.0';
+  $style_css = wp_remote_get("https://raw.githubusercontent.com/erm2587/".$theme_uri_slug."/master/style.css")['body'];
+    if ( preg_match( '/^[ \t\/*#@]*' . preg_quote( 'Version', '/' ) . ':(.*)$/mi', $style_css, $match ) && $match[1] )
+        $remote_version = _cleanup_header_comment( $match[1] );
+
+        if (version_compare($theme_data->version, $remote_version, '<')) {
+            $transient->response[$theme_slug] = array(
+              'theme'       => $theme_slug,
+              'new_version' => $remote_version,
+              'url'         => 'https://github.com/aldakur/'.$theme_uri_slug,
+              'package'     => 'https://github.com/aldakur/'.$theme_uri_slug.'/archive/master.zip',
+            );
+          }
+          return $transient;
+    }
+add_filter( 'pre_set_site_transient_update_themes', 'geko_check_update' );
 
 
 ?>
